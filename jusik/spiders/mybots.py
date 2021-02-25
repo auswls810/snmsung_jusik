8080a3608e81646c5af08b585c39619126562e34
auswls810 <67403194+auswls810@users.noreply.github.com>
2021-02-25 17:10:09 +0900

    Update mybots.py
    not for

--- jusik/spiders/mybots.py
+++ jusik/spiders/mybots.py
@@ -9,17 +9,14 @@ class MybotsSpider(scrapy.Spider):
 
     def parse(self, response):
         codes =  response.xpath('//*[@id="middle"]/div[1]/div[1]/div/span[1]/text()').extract()
-        prices = response.xpath('//*[@id="chart_area"]/div[1]/div/p[1]/em/span[1]/text()').extract()
-        totals = response.xpath('//*[@id="content"]/div[2]/div[1]/table/tbody/tr[3]/td[2]/em/text()').extract()
         blinds = response.css('.blind::text').extract()
 
         items = []
-        for idx in range(len(codes)):
-            item = JusikItem()
-            item['code']=codes[idx]
-            item['price'] = prices[idx]
-            item['total']= totals[idx]
-            item['high'] = blinds[idx * 40 +40]
-            item['low'] = blinds[idx * 40 +44]
-            items.append(item)
+        item = JusikItem()
+        item['code']=codes[0]
+        item['price'] = blinds[36]
+        item['total']= blinds[42]
+        item['high'] = blinds[40]
+        item['low'] = blinds[44]
+        items.append(item)
         return items
